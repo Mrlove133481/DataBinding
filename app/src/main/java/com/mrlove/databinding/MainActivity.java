dd3c842ef929d0ccdd5ca8ea15343f97961e562e
Mrlove133481 <1334819077@qq.com>
2020-01-22 14:17:19 +0800

    完成

--- app/src/main/java/com/mrlove/databinding/MainActivity.java
+++ app/src/main/java/com/mrlove/databinding/MainActivity.java
@@ -1,14 +1,26 @@
 package com.mrlove.databinding;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.mrlove.databinding.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    MyViewModel myViewModel;
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);  //绑定布局文件，之后binding就可以调用布局文件中的控件而不需要通过findviewbyid
+        // myViewModel = ViewModelProviders.of(this).get(MyViewModel.class); //得到一个实现了ViewModel的类
+        myViewModel = new ViewModelProvider(this).get(MyViewModel.class);  //   ViewModelProviders.of(this)
+        binding.setData(this.myViewModel); //为布局文件中的data绑定myViewModel
+        binding.setLifecycleOwner(this);  //实现观察者的作用，省略后UI数据不会自动刷新
+
+
     }
 }
